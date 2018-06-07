6d07c923b2aa49a96a2c1b8bdd1e075d576abcc3
Masa <masa.bozovic@email.com>
2018-06-07 19:00:42 +0200

    Television

---
+++ src/media/Television.java
@@ -0,0 +1,37 @@
+
+package media;
+
+
+public class Television {
+    
+    private int volume;
+    private int currentProgram;
+    private String turnOn;
+    
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getCurrentProgram() {
+        return currentProgram;
+    }
+
+    public void setCurrentProgram(int currentProgram) {
+        this.currentProgram = currentProgram;
+    }
+
+    public String getTurnOn() {
+        return turnOn;
+    }
+
+    public void setTurnOn(String turnOn) {
+        this.turnOn = turnOn;
+    }
+    
+    
+    
+}
