48d1fc7a5c8cb99f6fa06dca9cbfa68c0c9f535a
Masa <masa.bozovic@email.com>
2018-06-07 19:15:07 +0200

    Radio

--- src/media/Radio.java
+++ src/media/Radio.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package media;
 
-/**
- *
- * @author qa-popodne
- */
+
 public class Radio {
+    
+    private double fmFrequency;
+    private int amFrequency;
+    private char band;
+   
+    public double getFmFrequency() {
+        return fmFrequency;
+    }
+
+    public void setFmFrequency(double fmFrequency) {
+        this.fmFrequency = fmFrequency;
+    }
+
+    public int getAmFrequency() {
+        return amFrequency;
+    }
+
+    public void setAmFrequency(int amFrequency) {
+        this.amFrequency = amFrequency;
+    }
+
+    public char getBand() {
+        return band;
+    }
+
+    public void setBand(char band) {
+        this.band = band;
+    }
+    
+    
+    
     
 }
